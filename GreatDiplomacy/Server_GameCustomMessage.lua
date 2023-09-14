fe90111686a9b38a6afddb253b2c0af67124e47b
Gabriele Bini <gabrielebini9039@gmail.com>
2023-09-14 16:55:34 +0200

    hi

--- GreatDiplomacy/Server_GameCustomMessage.lua
+++ GreatDiplomacy/Server_GameCustomMessage.lua
@@ -2,6 +2,7 @@ require("consolelog")
 
 function Server_GameCustomMessage(game, ID, payload)
     if(payload.Mod == 8063520)then
+	    print("started")
         actualPl = payload.Content
         id = next(actualPl)
         n = actualPl[id]
@@ -10,28 +11,25 @@ function Server_GameCustomMessage(game, ID, payload)
         local playersStatus = publicGameData.PlayersStatus
         local Lvl = playersStatus[ID][id]
         if(n==0)then
-            playerGameData = reset()
+            playerGameData[ID][id] =  nil
+            playerGameData[id][ID] =  nil
         elseif(n==1)then
             playerGameData[ID][id] = (Lvl +1)
             playerGameData[id][ID] =  nil
         elseif(n==-1)then
             if(playerGameData[ID][id] == -1)then
-                playerGameData = reset() 
+                playerGameData[ID][id] =  nil 
                 playersStatus[ID][id] = (Lvl -1)
                 playersStatus[id][ID] = (Lvl -1)
                 publicGameData.PlayersStatus = playersStatus
                 Mod.PublicGameData = publicGameData
             else
                 playerGameData[id] = {[ID] = -1}
+                playerGameData[ID] = {[id] = Lvl-1}
             end
         end
 
-        function reset()
-            playerGameData[ID][id] =  nil
-            playerGameData[id][ID] =  nil
-            return(playerGameData)
-        end
-
         Mod.PlayerGameData = playerGameData
+        tblprint(Mod.PlayerGameData)
     end
 end
