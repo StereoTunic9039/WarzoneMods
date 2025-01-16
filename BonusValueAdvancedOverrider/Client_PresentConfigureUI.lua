2513ff7b7b46401695295f4195dda721aab3ee48
Gabriele Bini <gabrielebini9039@gmail.com>
2025-01-16 21:58:22 +0100

    trust mee

--- BonusValueAdvancedOverrider/Client_PresentConfigureUI.lua
+++ BonusValueAdvancedOverrider/Client_PresentConfigureUI.lua
@@ -23,7 +23,7 @@ function Client_PresentConfigureUI(rootParent)
 	SetWindow(f1Win)
 
 	local sliderLvlRelationsPc = CreateHorizontalLayoutGroup(phaseCont)			  -- Slider
-	labelLvlRelationsPc = CreateLabel(sliderLvlRelationsPc).SetText("You're on phase 1, check below to activate phase 2")			  -- Label
+	labelLvlRelationsPc = CreateLabel(sliderLvlRelationsPc).SetText("You're on PHASE 1, use custom scenario distribution to deploy on each territory as many armies as the bonus for that territory (alone) should be worth. \n\n\ncheck below to activate phase 2")			  -- Label
 		CreateEmpty(phaseCont)
 		CreateEmpty(phaseCont)
 		CreateEmpty(phaseCont)
@@ -38,13 +38,28 @@ function Client_PresentConfigureUI(rootParent)
 
 	SetWindow(mainWin);
 
+	function confirm()
+		
+		printSomething()
+		local inputCode = codeInputField.GetValue() -- getting month value
+		--if inputCode == nil or string.len(inputCode) == 0 then alert("Mod set up failed\nInput Code is absent") return
+		--else 
+		--	Mod.Settings.inputCode = inputCode
+		--end 
+
+	end
+
+	function printSomething()
+		print("something")
+	end
+
 	function phase1Fnt()
 		f1Win = "Fase1Window"
 		AddSubWindow(mainWin, f1Win);
 		SetWindow(f1Win)
 
 		local sliderLvlRelationsPc = CreateHorizontalLayoutGroup(phaseCont)			  -- Slider
-		labelLvlRelationsPc = CreateLabel(sliderLvlRelationsPc).SetText("You're on phase 1, check below to activate phase 2")			  -- Label
+		labelLvlRelationsPc = CreateLabel(sliderLvlRelationsPc).SetText("You're on PHASE 1, check below to activate phase 2")			  -- Label
 		CreateEmpty(phaseCont)
 
 		CreateCheckBox(phaseCont).SetIsChecked(false).SetText("Phase 2").SetOnValueChanged(function(IsChecked) showedreturnmessage = false; phase2Fnt(IsChecked) end)
@@ -73,7 +88,7 @@ function Client_PresentConfigureUI(rootParent)
 			.SetCharacterLimit(1000)  -- Both united in the selection for Players relations
 			CreateEmpty(phaseCont)
 			CreateEmpty(phaseCont)
-			confirmButton = CreateButton(phaseCont).SetText("Confirm")
+			CreateButton(phaseCont).SetText("Confirm").SetOnClick(confirm)
 		else
 			SetWindow(f2Win)
 			DestroyWindow(f2Win, true)
--- BonusValueAdvancedOverrider/Client_SaveConfigureUI.lua
+++ BonusValueAdvancedOverrider/Client_SaveConfigureUI.lua
@@ -2,8 +2,8 @@ require("Client_PresentConfigureUI")
 
 function Client_SaveConfigureUI(alert)
 
-    local inputCode = codeInputField.GetValue() -- getting month value
-    if inputCode = nil or string.len(inputCode) = 0 then alert("Mod set up failed\nInput Code is absent") return
+    local inputCode = codeInputField.GetValue()
+    if inputCode == nil or string.len(inputCode) == 0 then alert("Mod set up failed\nInput Code is absent") return
     else 
         Mod.Settings.inputCode = inputCode
     end 
