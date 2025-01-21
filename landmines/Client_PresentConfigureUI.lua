81a681a54d60df910b887c26651e72d902d53755
Gabriele Bini <gabrielebini9039@gmail.com>
2025-01-21 18:31:29 +0100

    doing good but nothing concrete yet

--- landmines/Client_PresentConfigureUI.lua
+++ landmines/Client_PresentConfigureUI.lua
@@ -4,30 +4,90 @@ function Client_PresentConfigureUI(rootParent)
     local textcolor = GetColors().TextColor;
     local root = GetRoot();
 
-    local unitCost = Mod.Settings.UnitCost;
-    if unitCost == nil then unitCost = 10; end
+	mainWin = "mainWindow"
+	fcWin = "fixedCostWindow"
+	pcWin = "progressiveCostWindow"
+	AddSubWindow(mainWin, fcWin);
+	AddSubWindow(mainWin, pcWin);
+
+    local fixedCost = Mod.Settings.FixedCost;
+	if fixedCost == nil then fixedCost = true; end						-- if true then the cost of landmines is a fixed amount, otherwise it progressively increases 
+	local unitCost = Mod.Settings.UnitCost;
+    if unitCost == nil then unitCost = 10; end							-- fixed amount cost of landmines
+	local unitInitialCost = Mod.Settings.UnitInitialCost;
+    if unitInitialCost == nil then unitInitialCost = 10; end			-- fixed amount cost of landmines
+	local arithmeticIncrease = Mod.Settings.arithmeticIncrease;
+	if arithmeticIncrease == nil then arithmeticIncrease = true; end	-- if true then the cost of landmines is a fixed amount, otherwise it progressively increases 
+	local IncreaseCost = Mod.Settings.IncreaseCost;
+    if IncreaseCost == nil then IncreaseCost = 10; end					-- fixed amount cost of landmines
+	local typeDamage = Mod.Settings.typeDamage;
+	if typeDamage == nil then typeDamage = true; end					-- if true then the damage inflicted by landmines is a fixed amount, otherwise it's a % of the armies on the territory
     local damage = Mod.Settings.Damage;
-    if damage == nil then damage = 25; end
+    if damage == nil then damage = 25; end								-- fixed amount damage inflicted by landmines
     local maxUnits = Mod.Settings.MaxUnits;
-    if maxUnits == nil then maxUnits = 3; end
-    local guessCooldown = Mod.Settings.GuessCooldown;
-    if guessCooldown == nil then guessCooldown = 3; end
+    if maxUnits == nil then maxUnits = 3; end							-- maximum number of landmines that a player can position
+    local cooldown = Mod.Settings.cooldown;
+    if cooldown == nil then cooldown = false; end       				-- if true then there is a cooldown between each time a player positions a landmine
 
-    CreateLabel(root).SetText("The amount of gold a landmine will cost").SetColor(textcolor);
-    unitCostInput = CreateNumberInputField(root).SetSliderMaxValue(100).SetSliderMinValue(5).SetValue(unitCost);
+	function typeCost()
+		CreateLabel(root).SetText("Is the cost of a landmine fixed rather than progressive?").SetColor(textcolor);
+		CreateCheckBox(root).SetValue(fixedCost)
 
-    CreateEmpty(root).SetPreferredHeight(5);
-    
-    CreateLabel(root).SetText("The damage this unit will inflict upon exploding").SetColor(textcolor);
-    damageInput = CreateNumberInputField(root).SetSliderMaxValue(100).SetSliderMinValue(1).SetValue(damage);
-    
-    CreateEmpty(root).SetPreferredHeight(5);
-    
-    CreateLabel(root).SetText("The maximum number of landmines a player may control").SetColor(textcolor);
-    maxUnitsInput = CreateNumberInputField(root).SetSliderMaxValue(5).SetSliderMinValue(1).SetValue(maxUnits);
-    
-    CreateEmpty(root).SetPreferredHeight(5);
+		CreateEmpty(root).SetPreferredHeight(5);
+	end
 
-    CreateLabel(root).SetText("The number of turns a player has to wait when he guesses wrong").SetColor(textcolor);
-    guessCooldownInput = CreateNumberInputField(root).SetSliderMinValue(1).SetSliderMaxValue(10).SetValue(guessCooldown);
+	function fixedCost()
+		CreateLabel(root).SetText("The amount of gold a landmine will cost").SetColor(textcolor);
+		unitCostInput = CreateNumberInputField(root).SetSliderMaxValue(100).SetSliderMinValue(5).SetValue(unitCost);
+
+		CreateEmpty(root).SetPreferredHeight(5);
+	end
+
+	function progressiveCost()
+		CreateLabel(root).SetText("The initial amount of gold a landmine will cost").SetColor(textcolor);
+		unitInitialCostInput = CreateNumberInputField(root).SetSliderMaxValue(100).SetSliderMinValue(5).SetValue(unitInitialCost);
+
+		CreateEmpty(root).SetPreferredHeight(5);
+
+
+		CreateLabel(root).SetText("Is the increase in the cost of a landmine arithmetic (addition of X) rather than geometric (multiplication by X)?").SetColor(textcolor);
+		CreateCheckBox(root).SetValue(arithmeticIncrease)
+
+		CreateEmpty(root).SetPreferredHeight(5);
+
+
+		CreateLabel(root).SetText("The increase will be worth (this is X)").SetColor(textcolor);
+		IncreaseCostInput = CreateNumberInputField(root).SetSliderMaxValue(50).SetSliderMinValue(1).SetValue(IncreaseCost);
+
+		CreateEmpty(root).SetPreferredHeight(5);
+	end
+
+    
+	function typeDamage()
+		CreateLabel(root).SetText("Is the damage inflicted by a landmine a fixed number rather than %?").SetColor(textcolor);
+		CreateCheckBox(root).SetValue(typeDamage)
+
+		CreateEmpty(root).SetPreferredHeight(5);
+	end
+
+	function fixedDamage()
+		CreateLabel(root).SetText("The damage this unit will inflict upon exploding").SetColor(textcolor);
+		damageInput = CreateNumberInputField(root).SetSliderMaxValue(100).SetSliderMinValue(1).SetValue(damage);
+		
+		CreateEmpty(root).SetPreferredHeight(5);
+	end
+    
+	function maxUnits()
+		CreateLabel(root).SetText("The maximum number of landmines a player may control (set 0 if there should be no limit)").SetColor(textcolor);
+		maxUnitsInput = CreateNumberInputField(root).SetSliderMaxValue(5).SetSliderMinValue(0).SetValue(maxUnits);
+		
+		CreateEmpty(root).SetPreferredHeight(5);
+	end
+
+	function cooldown()
+		CreateLabel(root).SetText("Is there a cooldown between each time a player positions a landmine?").SetColor(textcolor);
+		CreateCheckBox(root).SetValue(cooldown)
+
+		CreateEmpty(root).SetPreferredHeight(5);
+	end
 end
--- landmines/Client_SaveConfigureUI.lua
+++ landmines/Client_SaveConfigureUI.lua
@@ -1,6 +1,7 @@
 function Client_SaveConfigureUI(alert)
 	Mod.Settings.UnitCost = unitCostInput.GetValue();
+    Mod.Settings.UnitInitialCost = unitInitialCostInput.GetValue();
+    Mod.Settings.IncreaseCost = IncreaseCostInput.GetValue();
     Mod.Settings.Damage = damageInput.GetValue();
     Mod.Settings.MaxUnits = maxUnitsInput.GetValue();
-    Mod.Settings.GuessCooldown = guessCooldownInput.GetValue();
 end
