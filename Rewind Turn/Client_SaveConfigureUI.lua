67dba84a25a1dc7ca546f2d3b1a96c012a1e3687
Gabriele Bini <gabrielebini9039@gmail.com>
2025-06-02 19:35:58 +0200

    tdgh

--- Rewind Turn/Client_SaveConfigureUI.lua
+++ Rewind Turn/Client_SaveConfigureUI.lua
@@ -38,6 +38,6 @@ function Client_SaveConfigureUI(alert, addCard)
 		Mod.Settings.RewindTurn.StartPieces = 100
 	end
 
-	local cardID = addCard("Rewind Turn Card", "you know", "RewindTurnCard.png", Mod.Settings.NumPieces, Mod.Settings.MinPieces, Mod.Settings.StartPieces, Mod.Settings.CardWeight);
+	Mod.Settings.RewindTurn.RewindTurnCard = addCard("Rewind Turn Card", "you know", "RewindTurnCard.png", Mod.Settings.NumPieces, Mod.Settings.MinPieces, Mod.Settings.StartPieces, Mod.Settings.CardWeight);
 
 end  				-- Sets straight every unwanted input and such 
