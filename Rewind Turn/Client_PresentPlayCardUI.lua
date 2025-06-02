b06464722088c774eb3baa9adaf02beae3d92372
Gabriele Bini <gabrielebini9039@gmail.com>
2025-06-02 17:58:59 +0200

    idkk

--- Rewind Turn/Client_PresentPlayCardUI.lua
+++ Rewind Turn/Client_PresentPlayCardUI.lua
@@ -4,32 +4,5 @@ require('Utilities')
 function Client_PresentPlayCardUI(game, cardInstance, playCard, closeCardsDialog)
     Game = game;
 
-    --If this dialog is already open, close the previous one. This prevents two copies of it from being open at once which can cause errors due to only saving one instance of TargetTerritoryBtn
-    if (Close ~= nil) then
-        Close();
-    end
-    
-    if (WL.IsVersionOrHigher("5.34")) then --closeCardsDialog callback did not exist prior to 5.34
-        closeCardsDialog();
-    end
-
-    --If your mod has multiple cards, you can look at game.Settings.Cards[cardInstance.CardID].Name to see which one was played
-    game.CreateDialog(function(rootParent, setMaxSize, setScrollable, game, close)
-        Close = close;
-        setMaxSize(400, 200);
-        local vert = UI.CreateVerticalLayoutGroup(rootParent).SetFlexibleWidth(1); --set flexible width so things don't jump around while we change InstructionLabel
-
-        TargetTerritoryBtn = UI.CreateButton(vert).SetText("Select Territory").SetOnClick(TargetTerritoryClicked);
-        TargetTerritoryInstructionLabel = UI.CreateLabel(vert).SetText("");
-
-        UI.CreateButton(vert).SetText("Play Card").SetOnClick(function() 
-
-            local annotations = nil
-            annotations = { [TargetTerritoryID] = WL.TerritoryAnnotation.Create("Smoke Bomb") };
-            if (playCard("This " .. TargetTerritoryName, "SmokeBomb_" .. TargetTerritoryID, WL.TurnPhase.Deploys, annotations)) then
-                close();
-            end
-        end);
-    end);
+    playCard("Detonate a smoke bomb on nowhere" , "SmokeBomb", WL.TurnPhase.Deploys)
 end
-
