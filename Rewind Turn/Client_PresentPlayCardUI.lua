70c9a660a23268592e04bfcae8af1a0f8c1c3326
Gabriele Bini <gabrielebini9039@gmail.com>
2025-06-03 22:49:41 +0200

    big changes

--- Rewind Turn/Client_PresentPlayCardUI.lua
+++ Rewind Turn/Client_PresentPlayCardUI.lua
@@ -1,11 +1,13 @@
 require('Utilities')
 
---Called when the player attempts to play your card.  You can call playCard directly if no UI is needed, or you can call game.CreateDialog to present the player with options.
+--Called when the player attempts to play your card.  It creates the order with the name of the one who used the card
 function Client_PresentPlayCardUI(game, cardInstance, playCard, closeCardsDialog)
     Game = game;
 
     local pl = Game.Us.ID
     local player = Game.Game.Players[pl]
     local name = player.DisplayName(nil, true)
-    playCard(name .. "played a rewind card." , "RewindCard", WL.TurnPhase.CardsWearOff)
+    playCard(name .. " played a rewind card." , "RewindCard_" .. pl, WL.TurnPhase.CardsWearOff)
 end
+
+
--- Rewind Turn/Server_AdvanceTurn.lua
+++ Rewind Turn/Server_AdvanceTurn.lua
@@ -1,143 +1,57 @@
+require("Utilities");
 require("consolelog")
 
-function Server_AdvanceTurn_End(game, addNewOrder)
+function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)   -- when the order of the played card is called idk im too tired to properly comment goodday
+    if (order.proxyType == 'GameOrderPlayCardCustom' and startsWith(order.ModData, "RewindCard")) then
+        local playerID = tonumber(string.sub(order.ModData, 11));
+		
+		local pub = Mod.PublicGameData;
+		local allIDs = pub.RwcIDs;
 
-	local territories = game.ServerGame.LatestTurnStanding.Territories 
-	local alreadyChecked = {}			-- To avoid checking territories twice in the advanced version
-	WB = Mod.Settings.WeakenBlockades
-	--tblprint(game.ServerGame.Game.Players)
+		if allIDs == nil then
+			allIDs = {}
 
-	function baseVersion(tid, nterritory)
-		connectedTerritories = game.Map.Territories[tid].ConnectedTo
-		connectedTerritoriesOwners = {}
-		for ID, _ in pairs(connectedTerritories) do		-- checks who are all the owners of the bordering territories
-			if not connectedTerritoriesOwners[territories[ID].OwnerPlayerID] then
-				connectedTerritoriesOwners[territories[ID].OwnerPlayerID] = true
-			end
-		end
-		local length = 0
-		for _ in pairs(connectedTerritoriesOwners) do
-			length = length + 1
-		end			-- counts how many owners there are  (It's a bit inefficent and could be optimized, but it shouldn't be that heavy anyway)
-		if(length == 1 and ( next(connectedTerritoriesOwners) ~= WL.PlayerID.Neutral))then			-- if there is only one owner and they're a player
-			local negArmies			-- the armies that'll be removed
-			if(WB.percentualOrFixed)then	
-				if(WB.fixedArmiesRemoved > nterritory.NumArmies.NumArmies)then		-- if the armies removed are more than the armies on the territory 
-					negArmies = -nterritory.NumArmies.NumArmies						-- remove a total of armies equal to the armies on the territory
-				else																-- else
-					negArmies = -WB.fixedArmiesRemoved								-- remove the presetted amount of armies
-				end
-			else
-				negArmies = -((nterritory.NumArmies.NumArmies * WB.percentualArmiesRemoved) / 100)		-- remove the % amount 
-			end
-			local decrement = WL.TerritoryModification.Create(tid);		
-			decrement.AddArmies = negArmies 
-			local reduction = WL.GameOrderEvent.Create(next(connectedTerritoriesOwners), "Decrease armies in " .. game.Map.Territories[tid].Name, {}, {decrement});
-			local terr = game.Map.Territories[tid];
-			reduction.JumpToActionSpotOpt = WL.RectangleVM.Create(terr.MiddlePointX, terr.MiddlePointY, terr.MiddlePointX, terr.MiddlePointY)
-			addNewOrder(reduction)			-- all the creating order and whatnot
-		end  
-	end
-	
-	function advancedVersion(tid, nterritory)
-		if not alreadyChecked[tid] then
-			groupNeutrals = {[tid] = true}
-			terrToCheck = {}
-			pbg = nil  	-- the player bordering the chunk of territories
-			local result = thaSearch(tid, 0)
-			if(result ~= 0 and terrToCheck ~= nil and terrToCheck ~= {})then
-				for id, _ in pairs(terrToCheck) do
-					if not groupNeutrals[id] then
-						result = -1;
-						break;
-					end
-				end
-			end
-			if(result ~= 0 and result ~= -1)then
-				result = 1
-			end
-			if(result == 1)then
-				for id, _ in pairs(groupNeutrals) do
-					if(WB.percentualOrFixed)then
-						if(WB.fixedArmiesRemoved > territories[id].NumArmies.NumArmies)then
-							negArmies = -territories[id].NumArmies.NumArmies
-						else
-							negArmies = -WB.fixedArmiesRemoved
-						end
-					else
-						negArmies = -((territories[id].NumArmies.NumArmies * WB.percentualArmiesRemoved) / 100)
-					end
-					local decrement = WL.TerritoryModification.Create(id);
-					decrement.AddArmies = negArmies 
-					local reduction
-					if pbg == nil then
-						reduction = WL.GameOrderEvent.Create(WL.PlayerID.Neutral, "Decrease armies in " .. game.Map.Territories[id].Name, {}, {decrement});
-					else 
-						reduction = WL.GameOrderEvent.Create(pbg, "Decrease armies in " .. game.Map.Territories[id].Name, {}, {decrement});
-					end
-					local terr = game.Map.Territories[tid];
-					reduction.JumpToActionSpotOpt = WL.RectangleVM.Create(terr.MiddlePointX, terr.MiddlePointY, terr.MiddlePointX, terr.MiddlePointY)
-					addNewOrder(reduction)
-					alreadyChecked[id] = true
-				end
-			elseif(result == 0)then
-				if(terrToCheck ~= nil and terrToCheck ~= {})then
-					for id, _ in pairs(terrToCheck) do
-						alreadyChecked[id] = true
-					end
-				end
-				for id, _ in pairs(groupNeutrals) do
-					alreadyChecked[id] = true
-				end
-			end
-		end
-	end
+			local GameStanding = game.ServerGame.LatestTurnStanding
+			tblprint(GameStanding)
 
-	function thaSearch(tid, depth)
-		if(depth >= 3)then
-			terrToCheck[tid] = true
-		else
-			groupNeutrals[tid] = true
-			local connectedTerritories = game.Map.Territories[tid].ConnectedTo
-			for ID, cterritory in pairs(connectedTerritories) do
-				if territories[ID].OwnerPlayerID == WL.PlayerID.Neutral then		-- if neutral
-					if not alreadyChecked[ID] then-- and we haven't seen him yet
-						if(WB.appliesToAllNeutrals or WB.appliesToMinArmies <= territories[ID].NumArmies.NumArmies)then
-							local result = thaSearch(ID, depth+1)
-							if(result == 0)then
-								return 0;
-							end
-						else
-							return 0;
-						end
-					else
-						return 0
-					end
-				elseif pbg == territories[ID].OwnerPlayerID then	-- if we already saw them
-																		-- then just keep going	
-				elseif pbg ~= nil then								-- if there is someone else we already saw (that isn't the dude from the previous if)
-					return 0;											-- then fuck everything 
-				else												-- else
-					pbg = territories[ID].OwnerPlayerID					-- they'll be the one we "already saw" in the future
-				end
-			end
-		end
-	end
 
-	if game.ServerGame.Game.TurnNumber > WB.delayFromStart then	-- just a good load of checking if the territory meets the mod's criterias
-		for tid, nterritory in pairs(territories) do	
-			if(nterritory.IsNeutral)then
-				if(WB.appliesToAllNeutrals or WB.appliesToMinArmies <= nterritory.NumArmies.NumArmies)then
-					if(WB.ADVANCEDVERSION)then
-						advancedVersion(tid, nterritory)
-					else
-						baseVersion(tid, nterritory)
-					end
-				end
-			end
+
 		end
-	end
-			
+
+		table.insert(allIDs, playerID);
+		pub.RwcIDs = allIDs;
+		Mod.PublicGameData = pub;
+
+		--local priority = 7000; -- between 6000 and 8999 means it won't obscure a player's own territories
+		--local fogMod = WL.FogMod.Create('Obscured by smoke bomb', WL.StandingFogLevel.Fogged, priority, terrs, nil);
+
+		--local event = WL.GameOrderEvent.Create(order.PlayerID, 'Detonated a smoke bomb', {});
+		--event.FogModsOpt = {fogMod};
+		--event.JumpToActionSpotOpt = WL.RectangleVM.Create(td.MiddlePointX, td.MiddlePointY, td.MiddlePointX, td.MiddlePointY);
+
+		--if (WL.IsVersionOrHigher("5.34.1")) then
+		--	event.TerritoryAnnotationsOpt = { [targetTerritoryID] = WL.TerritoryAnnotation.Create("Smoke Bomb") };
+		--end
+		
+		--addNewOrder(event);
+
+		--Store the ID so we can later disable it
+		
+    end
 end
 
-					
+
+function Server_AdvanceTurn_End(game, addNewOrder)
+	--If we have any existing rewind  mods, remove them
+	local pub = Mod.PublicGameData;
+	if (pub.RwcIDs == nil) then
+		return; 
+	end
+
+	--local event = WL.GameOrderEvent.Create(WL.PlayerID.Neutral, 'Smoke bombs dissipate', {});
+	--event.RemoveFogModsOpt = priv.FogModIDs;
+	--addNewOrder(event);
+
+	pub.RwcIDs = nil;
+	Mod.PublicGameData = pub;
+end
